9c61350816a36ec41db0a03d21b5fadff80e36d4
cnorden <git@gitlab.eecs.wsu.edu>
2020-10-15 16:32:14 -0700

    added code for bst in python

--- test_bst/a.py
+++ test_bst/a.py
@@ -1,22 +1,59 @@
-# PROJECT: LEARNING PYTHON BY WRITING A BST
-# AUTHOR: KARL NORDEN
 
-# Class definition:
-class BSN(object):
-    def __init__(self, data):
-        self.data = data
+class Node(object):
+    def __init__(self, key):
+        self.data = key
         self.left = None
         self.right = None
 
 
-class BST(object):
-    #def constructTree(self, r, size):
-    def insert(self, root, key):
-        
-        if not root:
-            return BSN(key)
-        elif key < root.data:
-            root.left = self.insert(root.left, key)
+class BST(object):    
+    def insert(self, parent, key):
+        if not parent:
+            return Node(key)
         else:
-            root.right = self.insert(root.right, key)
+            parent.left = self.insert(parent.left, key) if key < parent.data else parent.left
+            parent.right = self.insert(parent.right, key) if key > parent.data else parent.right
+        return parent
 
+    def printIN(self, parent):
+        if parent:
+            self.printIN(parent.left)
+            print("{0} ".format(parent.data), end="")
+            self.printIN(parent.right)
+
+    def printPRE(self, parent):
+        if parent:
+            print("{0} ".format(parent.data), end="")
+            self.printIN(parent.left)
+            self.printIN(parent.right)
+
+    def printPOST(self, parent):
+        if parent:
+            self.printIN(parent.left)
+            self.printIN(parent.right)
+            print("{0} ".format(parent.data), end="")
+
+
+
+tree_A = BST()
+root = None
+
+root = tree_A.insert(root, 5)
+root = tree_A.insert(root, 8)
+root = tree_A.insert(root, 30)
+root = tree_A.insert(root, 12)
+root = tree_A.insert(root, 27)
+root = tree_A.insert(root, 1)
+root = tree_A.insert(root, 7)
+
+print("In-Order Traversal of the constructed BST:")
+tree_A.printIN(root)
+print()
+
+print("Pre-Order Traversal:")
+tree_A.printPRE(root)
+print()
+
+print("Post-Order Traversal:")
+tree_A.printPOST(root)
+print()
