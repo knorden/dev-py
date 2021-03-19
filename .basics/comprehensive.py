33df47b4832b219745aa3ecd3028baa15df872f2
Karl Norden <knorden@github.com>
2021-03-19 09:00:31 -0700

    cleaned up this repo

--- .basics/comprehensive.py
+++ .basics/comprehensive.py
@@ -1,7 +1,7 @@
-d1 = {1:'a',2:'x',3:'y'}
+d1 = {1: 'a', 2: 'x', 3: 'y'}
 print(d1)
 
-d2 = {'a':1,'x':2,'y':3}
+d2 = {'a': 1, 'x': 2, 'y': 3}
 print(d2)
 
 print(d2['a'])
@@ -10,4 +10,5 @@ print(d2.get('y'))
 print(d2.get('t'))
 #print(d2['t']) # this produces an error instead of returning a None like the function .get(<key>)
 
-d = {'c':5, 'a':1, 'b': 2, 'd':1}
+d = {'c': 5, 'a': 1, 'b': 2, 'd': 1}
+
